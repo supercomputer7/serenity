304e9c63034ec9fbd588751aeee71e34ad550dfe
Liav A <liavalb@gmail.com>
2021-12-24 13:49:34 +0200

    fixup! Kernel+LibC: Implement support for getting information about TCP sockets

--- Kernel/API/POSIX/netinet/tcp.h
+++ Kernel/API/POSIX/netinet/tcp.h
@@ -6,83 +6,87 @@
 
 #pragma once
 
-#include <AK/Types.h>
+#include <Kernel/API/POSIX/sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define TCP_INFO 11 /* Information about this connection. */
 
 #define TCP_NODELAY 10
 
 struct tcp_info {
-    u8	tcpi_state;
-	u8	tcpi_ca_state;
-	u8	tcpi_retransmits;
-	u8	tcpi_probes;
-	u8	tcpi_backoff;
-	u8	tcpi_options;
-	u8	tcpi_snd_wscale : 4, tcpi_rcv_wscale : 4;
-	u8	tcpi_delivery_rate_app_limited:1, tcpi_fastopen_client_fail:2;
+    uint8_t	tcpi_state;
+	uint8_t	tcpi_ca_state;
+	uint8_t	tcpi_retransmits;
+	uint8_t	tcpi_probes;
+	uint8_t	tcpi_backoff;
+	uint8_t	tcpi_options;
+	uint8_t	tcpi_snd_wscale : 4, tcpi_rcv_wscale : 4;
+	uint8_t	tcpi_delivery_rate_app_limited:1, tcpi_fastopen_client_fail:2;
 
-	u32	tcpi_rto;
-	u32	tcpi_ato;
-	u32	tcpi_snd_mss;
-	u32	tcpi_rcv_mss;
+	uint32_t	tcpi_rto;
+	uint32_t	tcpi_ato;
+	uint32_t	tcpi_snd_mss;
+	uint32_t	tcpi_rcv_mss;
 
-	u32	tcpi_unacked;
-	u32	tcpi_sacked;
-	u32	tcpi_lost;
-	u32	tcpi_retrans;
-	u32	tcpi_fackets;
+	uint32_t	tcpi_unacked;
+	uint32_t	tcpi_sacked;
+	uint32_t	tcpi_lost;
+	uint32_t	tcpi_retrans;
+	uint32_t	tcpi_fackets;
 
 	/* Times. */
-	u32	tcpi_last_data_sent;
-	u32	tcpi_last_ack_sent;     /* Not remembered, sorry. */
-	u32	tcpi_last_data_recv;
-	u32	tcpi_last_ack_recv;
+	uint32_t	tcpi_last_data_sent;
+	uint32_t	tcpi_last_ack_sent;     /* Not remembered, sorry. */
+	uint32_t	tcpi_last_data_recv;
+	uint32_t	tcpi_last_ack_recv;
 
 	/* Metrics. */
-	u32	tcpi_pmtu;
-	u32	tcpi_rcv_ssthresh;
-	u32	tcpi_rtt;
-	u32	tcpi_rttvar;
-	u32	tcpi_snd_ssthresh;
-	u32	tcpi_snd_cwnd;
-	u32	tcpi_advmss;
-	u32	tcpi_reordering;
+	uint32_t	tcpi_pmtu;
+	uint32_t	tcpi_rcv_ssthresh;
+	uint32_t	tcpi_rtt;
+	uint32_t	tcpi_rttvar;
+	uint32_t	tcpi_snd_ssthresh;
+	uint32_t	tcpi_snd_cwnd;
+	uint32_t	tcpi_advmss;
+	uint32_t	tcpi_reordering;
 
-	u32	tcpi_rcv_rtt;
-	u32	tcpi_rcv_space;
+	uint32_t	tcpi_rcv_rtt;
+	uint32_t	tcpi_rcv_space;
 
-	u32	tcpi_total_retrans;
+	uint32_t	tcpi_total_retrans;
 
-	u64	tcpi_pacing_rate;
-	u64	tcpi_max_pacing_rate;
-	u64	tcpi_bytes_acked;    /* RFC4898 tcpEStatsAppHCThruOctetsAcked */
-	u64	tcpi_bytes_received; /* RFC4898 tcpEStatsAppHCThruOctetsReceived */
-	u32	tcpi_segs_out;	     /* RFC4898 tcpEStatsPerfSegsOut */
-	u32	tcpi_segs_in;	     /* RFC4898 tcpEStatsPerfSegsIn */
+	uint64_t	tcpi_pacing_rate;
+	uint64_t	tcpi_max_pacing_rate;
+	uint64_t	tcpi_bytes_acked;    /* RFC4898 tcpEStatsAppHCThruOctetsAcked */
+	uint64_t	tcpi_bytes_received; /* RFC4898 tcpEStatsAppHCThruOctetsReceived */
+	uint32_t	tcpi_segs_out;	     /* RFC4898 tcpEStatsPerfSegsOut */
+	uint32_t	tcpi_segs_in;	     /* RFC4898 tcpEStatsPerfSegsIn */
 
-	u32	tcpi_notsent_bytes;
-	u32	tcpi_min_rtt;
-	u32	tcpi_data_segs_in;	/* RFC4898 tcpEStatsDataSegsIn */
-	u32	tcpi_data_segs_out;	/* RFC4898 tcpEStatsDataSegsOut */
+	uint32_t	tcpi_notsent_bytes;
+	uint32_t	tcpi_min_rtt;
+	uint32_t	tcpi_data_segs_in;	/* RFC4898 tcpEStatsDataSegsIn */
+	uint32_t	tcpi_data_segs_out;	/* RFC4898 tcpEStatsDataSegsOut */
 
-	u64   tcpi_delivery_rate;
+	uint64_t   tcpi_delivery_rate;
 
-	u64	tcpi_busy_time;      /* Time (usec) busy sending data */
-	u64	tcpi_rwnd_limited;   /* Time (usec) limited by receive window */
-	u64	tcpi_sndbuf_limited; /* Time (usec) limited by send buffer */
+	uint64_t	tcpi_busy_time;      /* Time (usec) busy sending data */
+	uint64_t	tcpi_rwnd_limited;   /* Time (usec) limited by receive window */
+	uint64_t	tcpi_sndbuf_limited; /* Time (usec) limited by send buffer */
 
-	u32	tcpi_delivered;
-	u32	tcpi_delivered_ce;
+	uint32_t	tcpi_delivered;
+	uint32_t	tcpi_delivered_ce;
 
-	u64	tcpi_bytes_sent;     /* RFC4898 tcpEStatsPerfHCDataOctetsOut */
-	u64	tcpi_bytes_retrans;  /* RFC4898 tcpEStatsPerfOctetsRetrans */
-	u32	tcpi_dsack_dups;     /* RFC4898 tcpEStatsStackDSACKDups */
-	u32	tcpi_reord_seen;     /* reordering events seen */
+	uint64_t	tcpi_bytes_sent;     /* RFC4898 tcpEStatsPerfHCDataOctetsOut */
+	uint64_t	tcpi_bytes_retrans;  /* RFC4898 tcpEStatsPerfOctetsRetrans */
+	uint32_t	tcpi_dsack_dups;     /* RFC4898 tcpEStatsStackDSACKDups */
+	uint32_t	tcpi_reord_seen;     /* reordering events seen */
 
-	u32	tcpi_rcv_ooopack;    /* Out-of-order packets received */
+	uint32_t	tcpi_rcv_ooopack;    /* Out-of-order packets received */
 
-	u32	tcpi_snd_wnd;	     /* peer's advertised receive window after
+	uint32_t	tcpi_snd_wnd;	     /* peer's advertised receive window after
 				      * scaling (bytes)
 				      */
 };
@@ -110,3 +114,7 @@ enum {
 #define TCPI_OPT_ECN		8 /* ECN was negociated at TCP session init */
 #define TCPI_OPT_ECN_SEEN	16 /* we received at least one packet with ECT */
 #define TCPI_OPT_SYN_DATA	32 /* SYN-ACK acked data in SYN sent or rcvd */
+
+#ifdef __cplusplus
+}
+#endif
