21455f72de7f84568f274ea00502f77da49b25b4
Liav A <liavalb@gmail.com>
2022-03-12 13:54:29 +0200

    Kernel+Userland: Replace FramebufferDevice with DisplayConnector
    This commit involves a major step into making the graphics subsystem
    more robust and prepared for bare metal modesetting.
    
    All usage of FramebufferDevices are gone both in the Kernel and
    Userland. The DisplayConnector class is suited for both 2D operations,
    and for 3D acceleration if available. The DisplayConnector interface
    doesn't allow mmap from userspace (which WindowServer was the sole
    legitimate user of this interface), instead, it expects userspace to
    open a DisplayConnector (which is a CharacterDevice now) and write to
    it.
    
    Therefore, switching between text mode (console mode) and graphical mode
    is much more straightforward and there's a strict mechanism in place to
    ensure coherency when attempting to do so.
    
    Another good side-effect from this change is that all GraphicsAdapter
    classes are greatly simplified because the graphics interface is
    directly exposed over /dev/gpu/connectorX devices, which means that we
    don't need to "route" from the removed framebuffer device ioctl to the
    attached GraphicsAdapter objects to handle these requests anymore.
    
    To accommodate these changes, the kernel command line fbdev option is
    removed and is replaced with simple_graphics_mode option - which can
    force the Kernel to not try to initialize any graphics device but to
    only generate a generic handle of a VGA device. This reflects the fact
    that we can't enforce (and probably don't want to) prohibition on usage
    of graphical mode (which is not enabled until WindowServer uses a TTY
    device to enable it), but we can enforce usage of a multiboot bootloader
    framebuffer.
    
    In regard to 3D acceleration, by default the DisplayConnector class does
    not advertise for such capability. It's a specific driver responsibility
    to do this so userspace can "negotiate" about the used 3D command set
    that the hardware supports. Therefore, each driver that overrides the
    ioctl method is also responsible to call the standard DisplayConnector
    ioctl method at the end if no 3D ioctl being specified was matched
    against the overriden ioctl method.
    
    To reflect the previous paragraph, the VirtIO graphics driver was
    accommodated with the following changes - the GPU3DDevice class is
    removed and replaced with VirtIODisplayConnector. Most of the
    functionality is moved from the VirtIO GraphicsAdapter class to this new
    class. The ioctl interface of the VirtIO virgl commandset is kept in the
    VirtIODisplayConnector class. The VirtIO scanout structure is reduced to
    representing a DisplayConnector, so essentially the mapping between
    these objects is 1:1.

--- Kernel/Graphics/VGA/GenericDisplayConnector.h
+++ Kernel/Graphics/VGA/GenericDisplayConnector.h
@@ -17,17 +17,22 @@ namespace Kernel {
 
 class VGAGenericDisplayConnector
     : public DisplayConnector {
+    friend class DeviceManagement;
 
 public:
-    static NonnullOwnPtr<VGAGenericDisplayConnector> must_create_with_preset_resolution(PhysicalAddress framebuffer_address, size_t framebuffer_width, size_t framebuffer_height, size_t framebuffer_pitch);
-    static NonnullOwnPtr<VGAGenericDisplayConnector> must_create();
-
-    void enable_console();
-    void disable_console();
+    static NonnullRefPtr<VGAGenericDisplayConnector> must_create_with_preset_resolution(PhysicalAddress framebuffer_address, size_t framebuffer_width, size_t framebuffer_height, size_t framebuffer_pitch);
+    static NonnullRefPtr<VGAGenericDisplayConnector> must_create();
 
     // ^DisplayConnector
     virtual bool modesetting_capable() const override { return false; }
     virtual bool double_framebuffering_capable() const override { return false; }
+    virtual bool flush_support() const override { return false; }
+    virtual bool partial_flush_support() const override { return false; }
+    // Note: Bare metal hardware probably require a defined refresh rate for modesetting.
+    // However, because this connector doesn't support such capability, this is safe
+    // to just advertise this as not supporting refresh rate of the connected display.
+    virtual bool refresh_rate_support() const override { return false; }
+
     virtual ErrorOr<ByteBuffer> get_edid() const override { return Error::from_errno(ENOTSUP); }
     virtual ErrorOr<void> set_resolution(Resolution const&) override { return Error::from_errno(ENOTSUP); }
     virtual ErrorOr<void> set_safe_resolution() override { return Error::from_errno(ENOTSUP); }
@@ -41,6 +46,12 @@ private:
     ErrorOr<void> create_attached_framebuffer_console();
     VGAGenericDisplayConnector(PhysicalAddress framebuffer_address, size_t framebuffer_width, size_t framebuffer_height, size_t framebuffer_pitch);
     VGAGenericDisplayConnector();
+
+    virtual ErrorOr<size_t> write_to_first_surface(u64 offset, UserOrKernelBuffer const&, size_t length) override;
+    virtual ErrorOr<void> flush_first_surface() override;
+
+    virtual void enable_console() override;
+    virtual void disable_console() override;
 
 protected:
     explicit VGAGenericDisplayConnector(PhysicalAddress framebuffer_address);
