3204c41e387bd8d74468132bce6f2ed376d924d1
Liav A <liavalb@gmail.com>
2022-07-13 20:07:33 +0300

    Kernel/Graphics: Remove unnecessary VGAConsole class abstraction
    The original intention was to support other types of consoles based on
    standard VGA modes, but it never came to an implementation, nor we need
    such feature at all.
    Therefore, this class is not needed and can be removed.

--- Kernel/Graphics/Console/TextModeConsole.h
+++ Kernel/Graphics/Console/TextModeConsole.h
@@ -12,7 +12,7 @@
 #include <Kernel/Locking/Spinlock.h>
 
 namespace Kernel::Graphics {
-class TextModeConsole final : public VGAConsole {
+class TextModeConsole final : public Console {
 public:
     static NonnullRefPtr<TextModeConsole> initialize();
     virtual size_t chars_per_line() const override { return width(); };
@@ -40,6 +40,7 @@ private:
 
     mutable Spinlock m_vga_lock;
 
+    NonnullOwnPtr<Memory::Region> m_vga_region;
     VirtualAddress m_current_vga_window;
 };
 
