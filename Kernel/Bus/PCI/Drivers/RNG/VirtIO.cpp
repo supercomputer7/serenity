30f054bd41524412f20aadc3d56bf4abe76a3163
Liav A. <liavalb@gmail.com>
2024-03-02 22:05:09 +0200

    Kernel/PCI: Add a driver for the PCI VirtIO RNG device

--- Kernel/Bus/PCI/Drivers/RNG/VirtIO.cpp
+++ Kernel/Bus/PCI/Drivers/RNG/VirtIO.cpp
@@ -6,11 +6,12 @@
 
 #include <Kernel/Bus/PCI/Access.h>
 #include <Kernel/Bus/PCI/Drivers/Driver.h>
+#include <Kernel/Bus/VirtIO/Transport/PCIe/TransportLink.h>
 #include <Kernel/Security/Random/VirtIO/RNG.h>
 
 namespace Kernel::PCI {
 
-class VirtIORNGDriver : public PCI::Driver {
+class VirtIORNGDriver final : public PCI::Driver {
 public:
     static void init();
 
@@ -21,6 +22,7 @@ public:
 
     virtual ErrorOr<void> probe(PCI::Device&) override;
     virtual void detach(PCI::Device&) override;
+    virtual ClassID class_id() const override { return PCI::ClassID::Legacy; }
     virtual Span<HardwareIDMatch const> matches() override;
 
 private:
@@ -43,20 +45,18 @@ void VirtIORNGDriver::detach(PCI::Device&)
 static HardwareIDMatch __matches[] = {
     // QEMU VirtIO RNG device
     {
-        .class_code = Optional<ClassCode> {},
         .subclass_code = Optional<SubclassCode> {},
         .revision_id = Optional<RevisionID> {},
         .hardware_id = HardwareID {
             PCI::VendorID::RedHat,
             PCI::DeviceID::VirtIOEntropy,
         },
-        .subsystem_id = Optional<SubsystemID> {},
-        .subsystem_vendor_id = Optional<SubsystemVendorID> {},
+        .subsystem_id_match = Optional<HardwareIDMatch::SubsystemIDMatch> {},
+        .programming_interface = Optional<ProgrammingInterface> {},
     },
-}
+};
 
-Span<HardwareIDMatch const>
-VirtIORNGDriver::matches()
+Span<HardwareIDMatch const> VirtIORNGDriver::matches()
 {
     return __matches;
 }
@@ -64,7 +64,7 @@ VirtIORNGDriver::matches()
 void VirtIORNGDriver::init()
 {
     auto driver = MUST(adopt_nonnull_ref_or_enomem(new VirtIORNGDriver()));
-    PCI::Access::register_driver(driver);
+    PCI::Access::the().register_driver(driver);
 }
 
 PCI_DEVICE_DRIVER(VirtIORNGDriver);
